92fe4d6c4a635b3cfd34ccd9e6df5955271b3b12
soufiane2123 <soufianriadi2123@gmail.com>
2023-10-02 14:13:38 +0100

    erc20 indexer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,49 +1,74 @@
-import { ConnectWallet } from "@thirdweb-dev/react"
-import Link from "next/link"
-import styles from "../styles/Header.module.css"
-import { useRouter } from "next/router"
+import { ConnectWallet } from "@thirdweb-dev/react";
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
+import { useRouter } from "next/router";
 
 const Header: React.FC = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-    return (
-        <div className={styles.container}>
-            <div>
-                <Link legacyBehavior href={"/"}>
-                    <a className={router.pathname == "/" ? styles.active : styles.link}>Mint</a>
-                </Link>
-                <Link legacyBehavior href={"/nfts"}>
-                    <a className={router.pathname == "/nfts" ? styles.active : styles.link}>NFTs</a>
-                </Link>
-                <Link legacyBehavior href={"/my-nfts"}>
-                    <a className={router.pathname == "/my-nfts" ? styles.active : styles.link}>
-                        My NFTs
-                    </a>
-                </Link>
-                <Link legacyBehavior href={"/fetch"}>
-                    <a className={router.pathname == "/fetch" ? styles.active : styles.link}>
-                        Fetch NFTs
-                    </a>
-                </Link>
-                <Link legacyBehavior href={"/buyMeACoffee"}>
-                    <a className={router.pathname == "/buyMeACoffee" ? styles.active : styles.link}>
-                        Buy Me a Coffee
-                    </a>
-                </Link>
-                {/* <Link legacyBehavior href={"/mint"}>
-                    <a className={router.pathname == "/mint" ? styles.active : styles.link}>Mint</a>
-                </Link> */}
-            </div>
-            <div className={styles.connectWallet}>
-                {router.pathname === "/" ||
-                router.pathname === "/buyMeACoffee" ||
-                router.pathname === "/nfts" ||
-                router.pathname === "/fetch" ? null : (
-                    <ConnectWallet />
-                )}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div>
+        <Link legacyBehavior href={"/"}>
+          <a className={router.pathname == "/" ? styles.active : styles.link}>
+            Mint
+          </a>
+        </Link>
+        <Link legacyBehavior href={"/nfts"}>
+          <a
+            className={router.pathname == "/nfts" ? styles.active : styles.link}
+          >
+            NFTs
+          </a>
+        </Link>
+        <Link legacyBehavior href={"/my-nfts"}>
+          <a
+            className={
+              router.pathname == "/my-nfts" ? styles.active : styles.link
+            }
+          >
+            My NFTs
+          </a>
+        </Link>
+        <Link legacyBehavior href={"/fetch"}>
+          <a
+            className={
+              router.pathname == "/fetch" ? styles.active : styles.link
+            }
+          >
+            Fetch NFTs
+          </a>
+        </Link>
+        <Link legacyBehavior href={"/fetch-tokens"}>
+          <a
+            className={
+              router.pathname == "/fetch-tokens" ? styles.active : styles.link
+            }
+          >
+            Fetch Tokens
+          </a>
+        </Link>
+        <Link legacyBehavior href={"/buyMeACoffee"}>
+          <a
+            className={
+              router.pathname == "/buyMeACoffee" ? styles.active : styles.link
+            }
+          >
+            Buy Me a Coffee
+          </a>
+        </Link>
+      </div>
+      <div className={styles.connectWallet}>
+        {router.pathname === "/" ||
+        router.pathname === "/buyMeACoffee" ||
+        router.pathname === "/nfts" ||
+        router.pathname === "/fetch" ||
+        router.pathname === "/fetch-tokens" ? null : (
+          <ConnectWallet />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
